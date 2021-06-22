e3874364819b944f64863dc41ea9417c5195c3bc
Ahamed Kabeer A <84619475+kabeerahd@users.noreply.github.com>
2021-06-22 21:06:55 +0530

    Add files via upload

---
+++ basic/src/week1/QuotientAndRemainder.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+		int number1 = 150, number2 = 20;
+		int Quotient = number1 / number2;
+		int remainder = number1 % number2;
+		System.out.println("Quotient is :" + Quotient);
+		System.out.println("Remainder is :" + remainder );
+       
+	}
+
+}
---
+++ basic/src/week1/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int first = 100, second = 35;
+		System.out.println("--Before swap--");
+		System.out.println("first number is :" + first);
+		System.out.println("Second number is :" + second);
+		float temporary = first;
+		first = second;
+		second = (int) temporary;
+		System.out.println("-After swap--");
+		System.out.println("First number : " + first);
+		System.out.println("Second number is :" + second);
+		
+	}
+
+}
